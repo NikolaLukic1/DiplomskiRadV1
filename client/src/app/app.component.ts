2eadb77640a39647021682cdc220e0027dd2e6fe
Nikola Lukic <nik.lukic.93@gmail.com>
2020-07-21 13:13:10 +0200

    dodate funkcionalnosti korpe i kreiran checkout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IProduct } from './shared/models/product';
 import { IPagination } from './shared/models/pagination';
+import { BasketService } from './basket/basket.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,8 +11,16 @@ import { IPagination } from './shared/models/pagination';
 export class AppComponent implements OnInit {
    title = 'Prodavnica';
 
-   constructor() {}
+   constructor(private basketService: BasketService) {}
 
    ngOnInit(): void {
+     const basketId = localStorage.getItem('basket_id');
+     if (basketId){
+       this.basketService.getBasket(basketId).subscribe(() => {
+         console.log('initialised basket')
+       }, error => {
+         console.log(error);
+       });
+     }
   }
 }
